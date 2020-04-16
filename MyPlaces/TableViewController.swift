070667acd7299a9160e4f8d88c9ce8db7ecc631a
Zaitsev Alexey <a.zaitsev@connectome.ai>
2020-04-16 05:16:23 +0300

    cell update

--- MyPlaces/TableViewController.swift
+++ MyPlaces/TableViewController.swift
@@ -17,6 +17,7 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
  }
+    
 
     // MARK: - Table view data source
 
@@ -30,12 +31,18 @@ class TableViewController: UITableViewController {
         
         cell.textLabel?.text = restaurantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         
         return cell
     }
 
 
-   
+   // MARK: - Table View Delegate
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
+
 
     /*
     // MARK: - Navigation
