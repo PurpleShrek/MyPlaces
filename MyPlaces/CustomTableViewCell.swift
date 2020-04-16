6e5df1f49d2be9b20052a9506227723b5dbdd0b0
Zaitsev Alexey <a.zaitsev@connectome.ai>
2020-04-16 05:31:52 +0300

    custom cells

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Zaitsev Aleksey on 16.04.2020.
+//  Copyright © 2020 Zaitsev Aleksey. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+}
